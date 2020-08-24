3d5e5b17b0ca0afdde34e6f934a8646e933e5559
87johnboyle <john.hazukisan@gmail.com>
2020-08-24 23:39:35 +0100

    Image added

--- src/components/MPDetail.js
+++ src/components/MPDetail.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+
 const MPDetail = (props) => {
   if(!props.mp) return null;
   return(
     <div>
-      <h3>Name: {props.mp.DisplayAs}</h3>
+      <h3>{props.mp.DisplayAs}</h3>
+      <img src={'http://data.parliament.uk/membersdataplatform/services/images/MemberPhoto/'+props.mp.$.Member_Id} alt="MP"/>
       <h4>Constituency: {props.mp.MemberFrom}</h4>
       <h4>Party: {props.mp.Party[0]._}</h4>
+
     </div>
   );
 }
