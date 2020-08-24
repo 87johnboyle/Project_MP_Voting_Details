1f747887081e7fcb9bbc4461d7d2ea02156d7bae
87johnboyle <john.hazukisan@gmail.com>
2020-08-24 22:52:16 +0100

    Added relevant info upon select

--- src/components/MPDetail.js
+++ src/components/MPDetail.js
@@ -4,7 +4,9 @@ const MPDetail = (props) => {
   if(!props.mp) return null;
   return(
     <div>
-      <h3>{props.mp.DisplayAs}</h3>
+      <h3>Name: {props.mp.DisplayAs}</h3>
+      <h4>Constituency: {props.mp.MemberFrom}</h4>
+      <h4>Party: {props.mp.Party[0]._}</h4>
     </div>
   );
 }
