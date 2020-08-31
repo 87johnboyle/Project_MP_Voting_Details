2d18472e0443b89c4f79be0bc03f9fea1019cd81
87johnboyle <john.hazukisan@gmail.com>
2020-08-31 23:54:45 +0100

    Quick bug quashed

--- src/components/MPDetail2.js
+++ src/components/MPDetail2.js
@@ -5,13 +5,14 @@ class MPDetail2 extends Component {
 
 render(){
   if(!this.props.mp2) return null;
+  if(!this.props.vote2) return null;
   return(
     <div>
       <h3>{this.props.mp2.DisplayAs}</h3>
       <img src={'http://data.parliament.uk/membersdataplatform/services/images/MemberPhoto/'+this.props.mp2.$.Member_Id} alt="MP"/>
       <h4>Constituency: {this.props.mp2.MemberFrom}</h4>
       <h4>Party: {this.props.mp2.Party[0]._}</h4>
-      <p>Vote:{this.props.vote.ArrayOfMemberVotingRecord.MemberVotingRecord[0].MemberId}</p>
+      <p>Vote:{this.props.vote2.ArrayOfMemberVotingRecord.MemberVotingRecord[0].MemberId}</p>
     </div>
   );
 }
